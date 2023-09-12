848c9726bec77aec6d430e94dd87c1401f5ce401
Агафонова Татьяна Сергеевна <Agafonova.T.S@corp.kp11.ru>
2023-09-12 13:28:22 +0300

    init

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿
+using ConsoleApp1;
+
+animal cat = new animal();
+cat.name = "Barsik";
+cat.SetAge(12);
+cat.voice = "Meow";
+
+Console.WriteLine(cat.voice);
+Console.WriteLine(cat.name);
---
+++ animal.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class animal
+    {
+        public string name;
+        private int age;
+        public string voice;
+
+        public void SetAge(int age)
+        {
+            if (age > 0)
+            {
+                this.age = age;
+            }
+            else
+            {
+                Console.WriteLine("Недопустимый возраст");
+            }
+        }
+    }
+}
